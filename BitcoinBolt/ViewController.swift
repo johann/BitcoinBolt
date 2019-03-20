67f5bd7f458840ef06ea1ff3c9e02b41b6a1187d
Johann Kerr <johannkerr@gmail.com>
2019-03-20 15:41:26 -0400

    Initial Commit

---
+++ BitcoinBolt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitcoinBolt
+//
+//  Created by Johann Kerr on 3/20/19.
+//  Copyright © 2019 Johann Kerr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
