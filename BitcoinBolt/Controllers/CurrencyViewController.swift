11cf8e28c6e6a81a6ded000a1b8c97169a5e353f
Johann Kerr <johannkerr@gmail.com>
2019-03-27 08:19:19 +0100

    add view animations

--- BitcoinBolt/Controllers/CurrencyViewController.swift
+++ BitcoinBolt/Controllers/CurrencyViewController.swift
@@ -17,15 +17,47 @@ class CurrencyViewController: UIViewController {
     @IBOutlet weak var eurView: UIView!
     @IBOutlet weak var usdView: UIView!
     @IBOutlet weak var gbpView: UIView!
-
+    @IBOutlet weak var usdXLayout: NSLayoutConstraint!
+    @IBOutlet weak var eurXLayout: NSLayoutConstraint!
+    @IBOutlet weak var gbpXLayout: NSLayoutConstraint!
     var rates: [Currency: DatePrice] = [:]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpViews()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setUpViews()
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.animateViews()
+    }
+    
+    private func animateViews() {
+        UIView.animate(withDuration: 0.5, delay: 0.0, options: [.curveEaseOut], animations: {
+            self.usdXLayout.constant -= self.view.bounds.width
+            self.eurXLayout.constant += self.view.bounds.width
+            self.gbpXLayout.constant -= self.view.bounds.width
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+    }
+    
+    private func setUpViews() {
+        self.usdXLayout.constant += self.view.bounds.width
+        self.eurXLayout.constant -= self.view.bounds.width
+        self.gbpXLayout.constant += self.view.bounds.width
+        
         [self.eurView, self.usdView, self.gbpView].forEach { (view) in
             view.layer.cornerRadius = view.bounds.width * 0.5
         }
+        
         self.eurLabel.text = self.rates[Currency.EUR]?.priceWithCurrencyCode
         self.usdLabel.text = self.rates[Currency.USD]?.priceWithCurrencyCode
         self.gbpLabel.text = self.rates[Currency.GBP]?.priceWithCurrencyCode
